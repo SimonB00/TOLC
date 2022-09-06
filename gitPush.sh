f2d4fb563d84bf6ff0e382bbf9abdf9a09e12027
SimonB00 <simone.balducci00@gmail.com>
2022-09-06 21:39:20 +0200

    Aggiornato script

--- gitPush.sh
+++ gitPush.sh
@@ -1,10 +1,13 @@
-#!bin/bash
+#!bin/zsh
 
 echo "Write a commit message"
 
-read commitMessage'
+read commitMessage
 
 git add chimica.tex
+git add chimica.pdf
+git add mate.tex
+git add mate.pdf
 
 git commit -m "$commitMessage"
 
